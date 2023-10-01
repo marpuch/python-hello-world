5c48c4a1b48f8e383ef0bbc8cb3901f7cdb582ad
Marek Puchalski <marpuch@gmail.com>
2023-10-01 16:22:32 +0200

    Second lesson.

---
+++ 02_typy_podstawowe/02_typy_podstawowe.py
@@ -0,0 +1,31 @@
+# Chwilowo tylko najbardziej podstawowe typy.
+
+# Typ Boolowski - posiada dwie wartości: Prawda i Fałsz, a właściwie to True i False
+
+# Tym razem będziemy sobie kopiować poniższe linie bezpośrednio do REPLa aby sprawdzić ich działania:
+
+True
+False
+1 > 0
+1 <= 1
+
+# Aby sprawdzić typ wyrażenia możemy użyć funkcji type().
+
+type(True)
+type(1 > 0)
+
+# Typy numeryczne - chwilowo obowiązują nas dwa. Typ całkowitoliczbowy int i zmiennoprzecinkowy float.
+
+1 # to będzie int
+1.0 # to będzie float
+
+# Ciekawostka: jakiego typu będzie wyrażenie:
+
+1+1
+1.0 + 1
+1 / 2
+
+# Łańcucy znaków - te już znamy:
+
+'to jest łańcuch znaków'
+"to też jest łańcuch znaków"
